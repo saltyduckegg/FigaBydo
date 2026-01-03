537c7181c58e000443142082d50d90cce49e1964
saltyduckegg <826391423@qq.com>
2026-01-03 13:04:10 +0800

    feat: Implement serverless image caching and uploading with SHA-256 hashing.

--- esa/serverLess.js
+++ esa/serverLess.js
@@ -26,12 +26,12 @@ async function handleRequest(request) {
             const cachedResponse = await cache.get(imageUrlHttp);
 
             if (cachedResponse) {
-                alert("命中缓存:", imageUrlHttp);
+                // alert("命中缓存:", imageUrlHttp);
                 console.log("命中缓存:", imageUrlHttp);
                 makeLog(request);
                 return cachedResponse;
             } else {
-                alert("未命中缓存:", imageUrlHttp);
+                // alert("未命中缓存:", imageUrlHttp);
                 console.log("未命中缓存:", imageUrlHttp);
                 makeLog(request);
                 return new Response("提取不到图片 (Cache Miss)", {
